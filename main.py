065daee64f6ae3744b6f828e413efbb698d399da
Krishna Sai <kvelidanda@gmail.com>
2024-11-17 22:15:54 +0530

    small changes

--- main.py
+++ main.py
@@ -63,4 +63,5 @@ if __name__ == '__main__':
     nltk.download('punkt')
         
     # Start the Flask server
-    app.run(debug=True, port=5000, use_reloader=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
